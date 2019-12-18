fc76526bbe345c0d92b3c55a97af377adb731b42
Jessica Weber <58643745+JessicaMWeber@users.noreply.github.com>
2019-12-17 19:05:50 -0600

    Add files via upload

---
+++ MedsTakenHistory.cs
@@ -0,0 +1,75 @@
+﻿using Data;
+using System;
+
+namespace Medication_Tracker
+{
+    public class MedsTakenHistory : DataObject
+    {
+        #region Constructors
+
+        public MedsTakenHistory()
+        {
+            ID = 0;
+            LoadObject();
+        }
+
+        public MedsTakenHistory(int id)
+        {
+            ID = id;
+            LoadObject();
+        }
+
+        #endregion Constructors
+
+        #region Helper Methods
+
+        private void LoadObject()
+        {
+            BaseTable = "MedsTakenHistory";
+            Load();
+            DataRow = DataSet.Tables[0].Rows[0];
+        }
+
+        #endregion Helper Methods
+
+        #region Properties
+
+        public int PatientID
+        {
+            get
+            {
+                return Convert.ToInt32(DataRow["PatientID"]);
+            }
+            set
+            {
+                DataRow["PatientID"] = value;
+            }
+        }
+
+        public int MedID
+        {
+            get
+            {
+                return Convert.ToInt32(DataRow["MedID"]);
+            }
+            set
+            {
+                DataRow["MedID"] = value;
+            }
+        }
+
+        public DateTime TimeTaken
+        {
+            get
+            {
+                return Convert.ToDateTime(DataRow["TimeTaken"]);
+            }
+            set
+            {
+                DataRow["TimeTaken"] = value;
+            }
+        }
+
+        #endregion Properties
+    }
+}
