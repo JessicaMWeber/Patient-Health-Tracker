aad95cb0a52987571b51a86d130650345a8e0fdc
Jessica Weber <58643745+JessicaMWeber@users.noreply.github.com>
2019-12-17 18:50:07 -0600

    Delete MedsForPatient.sql

--- SQL Scripts/Tables/MedsForPatient.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE MedsForPatient
-(
-	ID int IDENTITY (1,1),
-	MedID int FOREIGN KEY REFERENCES Medications(ID),
-	PatientID int FOREIGN KEY REFERENCES Patient(ID),
-	Dose varchar(50),
-	[Route] varchar(100),
-	MedTime datetime2
-)
